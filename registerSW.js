892bf73d61df835e7e654b8bea7a1dca0c351db9
Mudassir Iqbal <mudassir.iqbal7860@gmail.com>
2025-09-22 16:20:17 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pwa-1-thing/sw.js', { scope: '/pwa-1-thing/' })})}
