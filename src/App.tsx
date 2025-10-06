80540379fc662629ed3e094fecf1dc38db2e8d61
Mudassir Iqbal <mudassir.iqbal7860@gmail.com>
2025-10-06 15:40:26 +0530

    remove password prompt

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [isStandalone, setIsStandalone] = useState(false);
-  const [confirmPassword, setConfirmPassword] = useState(false);
-  const password = import.meta.env.VITE_PASSWORD;
   const openPage = () => {
     const link = document.createElement("a");
     link.href = "https://1-thing.in/#/timesheet";
@@ -15,25 +13,17 @@ function App() {
     const handleMediaQueryChange = (e: MediaQueryListEvent) => {
       if (e.matches) {
         setIsStandalone(e.matches);
-        confirmPassword && openPage();
+        openPage();
       }
     };
 
     // Initial check
     if (mediaQuery.matches) {
-      const confirm = window.prompt("Confirm password");
-      setConfirmPassword(password === confirm);
       setIsStandalone(mediaQuery.matches);
-      password === confirm && openPage();
+      openPage();
     }
     mediaQuery.addEventListener("change", handleMediaQueryChange);
-    if (navigator.serviceWorker) {
-      navigator.serviceWorker.getRegistrations().then((registrations) => {
-        registrations.forEach((registration) => {
-          registration.unregister();
-        });
-      });
-    }
+
     // Clean up the event listener on component unmount
     return () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
@@ -49,7 +39,6 @@ function App() {
           height: "100%",
         }}
       >
-        {!confirmPassword && isStandalone && <p>Wrong password!</p>}
         {!isStandalone ? (
           <p
             style={{
@@ -61,13 +50,8 @@ function App() {
           </p>
         ) : (
           <>
-            {!confirmPassword ? (
-              <button onClick={() => window.location.reload()}>
-                Try Again
-              </button>
-            ) : (
-              <p>Loading...</p>
-            )}
+            <p>Loading...</p>
+            <button onClick={() => window.location.reload()}>Try Again</button>
           </>
         )}
       </div>
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
+const BASE_URL = "/pwa-1-thing/";
 export default defineConfig({
+  base: BASE_URL,
   plugins: [
     react(),
     VitePWA({
@@ -11,22 +13,25 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,jpg,jpeg}"],
+      },
       manifest: {
         name: "PWA 1-thing",
         short_name: "PWA 1-thing",
-        start_url: "/",
+        start_url: BASE_URL,
         display: "standalone",
         background_color: "#000000",
         theme_color: "#000000",
         orientation: "portrait",
         icons: [
           {
-            src: "/icon-192x192.png",
+            src: `${BASE_URL}icon-192x192.png`,
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/icon-512x512.png",
+            src: `${BASE_URL}icon-512x512.png`,
             sizes: "512x512",
             type: "image/png",
           },
